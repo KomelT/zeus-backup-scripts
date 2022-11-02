fd7bbda41f7b3e97b8a2517dd14186270b0fc3fa
Tilen Komel <tilen.komel10@gmail.com>
2022-11-02 14:15:46 +0100

    first commit

---
+++ main.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/bash
+
+date=$(date +"%y-%m-%d_%H:%M:%S")
+
+tmp_folder="/appdata/tmp"
+mkdir -p $tmp_folder
+
+echo "------ ${date} ------" >> "${tmp_folder}/logs.txt"
+echo "" >> "${tmp_folder}/logs.txt"
+
+content='{"content": "**'$(date +"%y-%m-%d %H:%M:%S")'**", "embeds": ['
+tmp=""
+
+/root/zeus-docker-backup-scripts/video-cdn-ad4u/script.sh
+if [[ $? -ne 0 ]]; then
+    echo "video-cdn-ad4u/script.sh exited with a non zero exit code" >> "${tmp_folder}/logs.txt"
+    tmp='{"title": "video-cdn-ad4u", "description": "NOK! Please check logs.", "color": 16711680}'
+else
+    echo "video-cdn-ad4u/script.sh exited with a zero exit code" >> "${tmp_folder}/logs.txt"
+    tmp='{"title": "video-cdn-ad4u", "description": "OK!", "color": 65280}'
+fi
+content="${content}${tmp}"
+
+echo "${content}]}" >> "${tmp_folder}/logs.txt"
+
+/usr/bin/curl -i -H "Content-Type: application/json" -d  "${content}]}" "${DISC_WEBHOOK_URL}" >> "${tmp_folder}/logs.txt"
---
+++ video-cdn-ad4u/script.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/bash
+
+echo $BACKB_KEY_ID >> /root/test.log 
+
+# backup name
+name="video-cdn-ad4u"
+
+date=$(date +"%y-%m-%d_%H:%M:%S")
+
+# create necessary folders
+tmp_folder="/appdata/tmp/${name}"
+mkdir -p "${tmp_folder}/${date}"
+
+# set-up logs
+mkdir -p "${tmp_folder}/logs"
+echo "------ ${date} ------" >> "${tmp_folder}/logs/logs.txt"
+echo "" >> "${tmp_folder}/logs/logs.txt"
+
+err=false
+
+# Backblaze b2 authorize
+b2 authorize-account $BACKB_KEY_ID $BACKB_APP_KEY >> "${tmp_folder}/logs/logs.txt"
+if [[ $? -ne 0 ]]; then
+    err=true
+fi
+
+# backup db to .sql
+docker exec cloud-komelt-dev_nextcloud-db-1 /usr/bin/mysqldump -u nextcloud --password="${NEXT_DB_PASS}" nextcloud > "${tmp_folder}/${date}/db_${date}.sql" 2>> "${tmp_folder}/logs/logs.txt" 
+if [[ $? -ne 0 ]]; then
+    err=true
+else
+    # if ok then upload to Backblaze bucket
+    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/db_${date}.sql" "${name}/${date}/db/db_${date}.sql" >> "${tmp_folder}/logs/logs.txt"
+    if [[ $? -ne 0 ]]; then
+        err=true
+    fi
+fi
+
+b2 upload-file zeus-docker-backup "/etc/nginx/conf.d/si.podjetni.video.conf" "${name}/${date}/nginx/nginx_si.podjetni.video_${date}.conf" >> "${tmp_folder}/logs/logs.txt"
+if [[ $? -ne 0 ]]; then
+    err=true
+fi
+
+echo "" >> "${tmp_folder}/logs/logs.txt"
+echo "" >> "${tmp_folder}/logs/logs.txt"
+
+if [[ $err == "true" ]]; then
+    exit 1
+fi
