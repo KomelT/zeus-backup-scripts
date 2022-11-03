91ca9e18aa2423fbfd94640a2169d2c26a2879d8
Tilen Komel <tilen.komel10@gmail.com>
2022-11-03 09:10:43 +0100

    added: nginx conf backup

--- video-cdn-ad4u/script.sh
+++ video-cdn-ad4u/script.sh
@@ -3,6 +3,8 @@
 # backup name
 name="video-cdn-ad4u"
 
+next_base="/appdata/cloud-komelt-dev_nextcloud/html"
+
 date=$(date +"%y-%m-%d_%H:%M:%S")
 
 # create necessary folders
@@ -22,6 +24,10 @@ if [[ $? -ne 0 ]]; then
     err=true
 fi
 
+
+# ------------------------------- BACKUP START
+
+
 # backup db to .sql
 docker exec cloud-komelt-dev_nextcloud-db-1 /usr/bin/mysqldump -u nextcloud --password="${NEXT_DB_PASS}" nextcloud > "${tmp_folder}/${date}/db_${date}.sql" 2>> "${tmp_folder}/logs/logs.txt" 
 if [[ $? -ne 0 ]]; then
@@ -34,10 +40,28 @@ else
     fi
 fi
 
+
+# backup nginx.conf
 b2 upload-file zeus-docker-backup "/etc/nginx/conf.d/si.podjetni.video.conf" "${name}/${date}/nginx/nginx_si.podjetni.video_${date}.conf" >> "${tmp_folder}/logs/logs.txt"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
+
+
+# backup nginx config folder
+zip -r "${tmp_folder}/${date}/conf_${date}.zip" "${next_base}/config" >> "${tmp_folder}/logs/logs.txt"
+if [[ $? -ne 0 ]]; then
+    err=true
+else
+    # if ok then upload to Backblaze bucket
+    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/conf_${date}.zip" "${name}/${date}/nextcloud/conf_${date}.zip" >> "${tmp_folder}/logs/logs.txt"
+    if [[ $? -ne 0 ]]; then
+        err=true
+    fi
+fi
+
+# ------------------------------- BACKUP END
+
 
 echo "" >> "${tmp_folder}/logs/logs.txt"
 echo "" >> "${tmp_folder}/logs/logs.txt"
