fee7aae981ab247ed2a8300a78082f111d1af174
Tilen Komel <tilen.komel10@gmail.com>
2022-11-03 11:52:20 +0100

    added: passbolt to main

--- main.sh
+++ main.sh
@@ -11,6 +11,7 @@ echo "" >> "${tmp_folder}/logs.txt"
 content='{"content": "**'$(date +"%y-%m-%d %H:%M:%S")'**", "embeds": ['
 tmp=""
 
+
 /root/zeus-docker-backup-scripts/video-cdn-ad4u/script.sh
 if [[ $? -ne 0 ]]; then
     echo "video-cdn-ad4u/script.sh exited with a non zero exit code" >> "${tmp_folder}/logs.txt"
@@ -19,7 +20,19 @@ else
     echo "video-cdn-ad4u/script.sh exited with a zero exit code" >> "${tmp_folder}/logs.txt"
     tmp='{"title": "video-cdn-ad4u", "description": "OK!", "color": 65280}'
 fi
+content="${content}${tmp},"
+
+
+/root/zeus-docker-backup-scripts/passbolt/script.sh
+if [[ $? -ne 0 ]]; then
+    echo "passbolt/script.sh exited with a non zero exit code" >> "${tmp_folder}/logs.txt"
+    tmp='{"title": "passbolt", "description": "NOK! Please check logs.", "color": 16711680}'
+else
+    echo "passbolt/script.sh exited with a zero exit code" >> "${tmp_folder}/logs.txt"
+    tmp='{"title": "passbolt", "description": "OK!", "color": 65280}'
+fi
 content="${content}${tmp}"
+
 
 echo "${content}]}" >> "${tmp_folder}/logs.txt"
 
