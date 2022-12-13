060847576a8d17197ed9240ae8d4ba3e196f4f40
Tilen Komel <tilen.komel10@gmail.com>
2022-12-13 11:41:41 +0100

    Added to passbolt "remove file older than 30 days"

--- passbolt/script.sh
+++ passbolt/script.sh
@@ -16,26 +16,11 @@ echo "" >> "${tmp_folder}/logs/logs.txt"
 
 err=false
 
-# Backblaze b2 authorize
-b2 authorize-account $BACKB_KEY_ID $BACKB_APP_KEY 2>> "${tmp_folder}/logs/logs.txt"
-if [[ $? -ne 0 ]]; then
-    err=true
-fi
-
-
 # ------------------------------- BACKUP START
-
-
 # backup db to .sql
 docker exec -i passbolt-db-1 mysqldump -u passbolt --password="${PASSB_DB_PASSWORD}" passbolt > "${tmp_folder}/${date}/passbolt-sqlbkp_${date}.sql" 2>> "${tmp_folder}/logs/logs.txt"
 if [[ $? -ne 0 ]]; then
     err=true
-else
-    # if ok then upload to Backblaze bucket
-    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/passbolt-sqlbkp_${date}.sql" "${name}/${date}/passbolt-sqlbkp_${date}.sql" 2>> "${tmp_folder}/logs/logs.txt"
-    if [[ $? -ne 0 ]]; then
-        err=true
-    fi
 fi
 
 
@@ -43,12 +28,6 @@ fi
 docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey_private.asc "${tmp_folder}/${date}/passbolt-srvkey-privatebkp_${date}.asc"
 if [[ $? -ne 0 ]]; then
     err=true
-else
-    # if ok then upload to Backblaze bucket
-    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/passbolt-srvkey-privatebkp_${date}.asc" "${name}/${date}/passbolt-srvkey-privatebkp_${date}.asc" >> "${tmp_folder}/logs/logs.txt"
-    if [[ $? -ne 0 ]]; then
-        err=true
-    fi
 fi
 
 
@@ -56,12 +35,6 @@ fi
 docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey.asc "${tmp_folder}/${date}/passbolt-srvkeybkp_${date}.asc"
 if [[ $? -ne 0 ]]; then
     err=true
-else
-    # if ok then upload to Backblaze bucket
-    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/passbolt-srvkeybkp_${date}.asc" "${name}/${date}/passbolt-srvkeybkp_${date}.asc" 2>> "${tmp_folder}/logs/logs.txt"
-    if [[ $? -ne 0 ]]; then
-        err=true
-    fi
 fi
 
 
@@ -72,27 +45,31 @@ if [[ $? -ne 0 ]]; then
 fi
 
 
-# backup docker-compose.yaml
-# b2 upload-file zeus-docker-backup "/home/komelt/git-repos/boilerplates/passbolt/production.yaml" "${name}/${date}/docker-composebkp_${date}.yaml" 2>> "${tmp_folder}/logs/logs.txt"
-# if [[ $? -ne 0 ]]; then
-#     err=true
-# fi
-
-
 # backup serverkey.asc
 docker cp passbolt-app-1:/usr/share/php/passbolt/plugins/PassboltCe/WebInstaller/templates/Config/passbolt.php "${tmp_folder}/${date}/passbolt-confbkp_${date}.php"
 if [[ $? -ne 0 ]]; then
     err=true
-else
-    # if ok then upload to Backblaze bucket
-    b2 upload-file zeus-docker-backup "${tmp_folder}/${date}/passbolt-confbkp_${date}.php" "${name}/${date}/passbolt-confbkp_${date}.php" 2>> "${tmp_folder}/logs/logs.txt"
-    if [[ $? -ne 0 ]]; then
-        err=true
-    fi
+fi
+# ------------------------------- BACKUP END
+
+# ------------------------------- SYNC FILES START
+
+# Remove directories that are older than 30 days
+find "${tmp_folder}"/* -mtime +30 -maxdepth 0 -exec basename {} \; | grep -v "logs" | xargs rm
+
+# Backblaze b2 authorize
+b2 authorize-account $BACKB_KEY_ID $BACKB_APP_KEY 2>> "${tmp_folder}/logs/logs.txt"
+if [[ $? -ne 0 ]]; then
+    err=true
 fi
 
+# Sync local cirectory with b2 bucket
+b2 sync --delete --compareVersions none "${tmp_folder}" "b2://zeus-docker-backup/${name}/" 2>> "${tmp_folder}/logs/logs.txt"
+if [[ $? -ne 0 ]]; then
+    err=true
+fi
 
-# ------------------------------- BACKUP END
+# ------------------------------- SYNC FILES END
 
 
 echo "" >> "${tmp_folder}/logs/logs.txt"
