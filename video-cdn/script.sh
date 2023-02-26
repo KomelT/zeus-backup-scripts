6b56397802b3ac876b93053a8a0e2b86cd28a654
Tilen Komel <tilen.komel10@gmail.com>
2023-02-26 13:19:45 +0100

    end of test

--- video-cdn/script.sh
+++ video-cdn/script.sh
@@ -22,25 +22,25 @@ err=false
 
 # ------------------------------- BACKUP START
 
-# docker exec --user www-data video-cdn-nextcloud-1 php occ maintenance:mode --on
+docker exec --user www-data video-cdn-nextcloud-1 php occ maintenance:mode --on
 
 
 # backup db to .sql
-# docker exec video-cdn-mariadb-1 mysqldump --single-transactio -u nextcloud --password="${NEXTCLOUD_DB_PASS}" nextcloud > "${tmp_folder}/${date}/nextcloud-sqlbkp_${date}.sql" 2>> "${tmp_folder}/${date}/log_${date}.log" 
+docker exec video-cdn-mariadb-1 mysqldump --single-transactio -u nextcloud --password="${NEXTCLOUD_DB_PASS}" nextcloud > "${tmp_folder}/${date}/nextcloud-sqlbkp_${date}.sql" 2>> "${tmp_folder}/${date}/log_${date}.log" 
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup nginx.conf
-# cp "/etc/nginx/conf.d/si.podjetni.video.conf" "${tmp_folder}/${date}/nginx-confbkp_${date}.conf" 2>> "${tmp_folder}/${date}/log_${date}.log"
+cp "/etc/nginx/conf.d/si.podjetni.video.conf" "${tmp_folder}/${date}/nginx-confbkp_${date}.conf" 2>> "${tmp_folder}/${date}/log_${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup nextcloud folder
-# zip -r "${tmp_folder}/${date}/nextcloud-dirbkp_${date}.zip" "${nextcloud_location}" 2>> "${tmp_folder}/${date}/log_${date}.log"
+zip -r "${tmp_folder}/${date}/nextcloud-dirbkp_${date}.zip" "${nextcloud_location}" 2>> "${tmp_folder}/${date}/log_${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
