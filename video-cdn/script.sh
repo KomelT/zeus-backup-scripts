05a5bf98f3b2093d02f00e3e68575cb6f8f8d2c4
Tilen Komel <tilen.komel10@gmail.com>
2023-02-27 10:13:40 +0100

    ip change

--- video-cdn/script.sh
+++ video-cdn/script.sh
@@ -67,12 +67,12 @@ cd "${tmp_folder}"
 
 find "${tmp_folder}"/* -mtime +30 -maxdepth 0 -exec basename {} \; | xargs rm -r {}
 
-ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.19 mkdir -p "/data/${name}/config/" 2>> "${tmp_folder}/logs/${date}.log"
+ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.18 mkdir -p "/data/${name}/config/" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
-rsync -au "/appdata/tmp/${name}/" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.19:"/data/${name}/config/" --delete 2>> "${tmp_folder}/logs/${date}.log"
+rsync -au "/appdata/tmp/${name}/" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.18:"/data/${name}/config/" --delete 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
@@ -86,22 +86,22 @@ fi
 day=$(date -d "2022-07-17" +%u)
 
 if [ "$day" == "7" ]; then
-    ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.19 mkdir -p "/data/${name}/data/weekly/" 2>> "${tmp_folder}/logs/${date}.log"
+    ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.18 mkdir -p "/data/${name}/data/weekly/" 2>> "${tmp_folder}/logs/${date}.log"
     if [[ $? -ne 0 ]]; then
         err=true
     fi
 
-    rsync -au "${nextcloud_location}/data" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.19:"/data/${name}/data/weekly/" --delete 2>> "${tmp_folder}/logs/${date}.log"
+    rsync -au "${nextcloud_location}/data" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.18:"/data/${name}/data/weekly/" --delete 2>> "${tmp_folder}/logs/${date}.log"
     if [[ $? -ne 0 ]]; then
         err=true
     fi
 else
-    ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.19 mkdir -p "/data/${name}/data/daily/" 2>> "${tmp_folder}/logs/${date}.log"
+    ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.18 mkdir -p "/data/${name}/data/daily/" 2>> "${tmp_folder}/logs/${date}.log"
     if [[ $? -ne 0 ]]; then
         err=true
     fi
 
-    rsync -au "${nextcloud_location}/data" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.19:"/data/${name}/data/daily/" --delete 2>> "${tmp_folder}/logs/${date}.log"
+    rsync -au "${nextcloud_location}/data" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.18:"/data/${name}/data/daily/" --delete 2>> "${tmp_folder}/logs/${date}.log"
     if [[ $? -ne 0 ]]; then
         err=true
     fi
