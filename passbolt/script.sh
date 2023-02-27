5e341690ef2a4674079021dfff1f3ab57778e3de
Tilen Komel <tilen.komel10@gmail.com>
2023-02-27 11:00:31 +0100

    rewrited passvolt backup

--- passbolt/script.sh
+++ passbolt/script.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/bash
 
-# backup name
+# service name
 name="passbolt"
 
 date=$(date +"%y-%m-%d_%H:%M:%S")
@@ -11,46 +11,52 @@ mkdir -p "${tmp_folder}/${date}"
 
 # set-up logs
 mkdir -p "${tmp_folder}/logs"
-echo "------ ${date} ------" >> "${tmp_folder}/${date}/log_${date}.log"
-echo "" >> "${tmp_folder}/${date}/log_${date}.log"
+echo "------ ${date} ------" >> "${tmp_folder}/logs/${date}.log"
+echo "" >> "${tmp_folder}/logs/${date}.log"
 
 err=false
 
+
+
 # ------------------------------- BACKUP START
+
 # backup db to .sql
-docker exec -i passbolt-db-1 mysqldump -u passbolt --password="${PASSB_DB_PASSWORD}" passbolt > "${tmp_folder}/${date}/passbolt-sqlbkp_${date}.sql" 2>> "${tmp_folder}/${date}/log_${date}.log"
+docker exec -i passbolt-db-1 mysqldump -u passbolt --password="${PASSBOLT_DB_PASSWORD}" passbolt > "${tmp_folder}/${date}/passbolt-sqlbkp.sql" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup serverkey_private.asc
-docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey_private.asc "${tmp_folder}/${date}/passbolt-srvkey-privatebkp_${date}.asc"
+docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey_private.asc "${tmp_folder}/${date}/passbolt-srvkey-privatebkp.asc" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup serverkey.asc
-docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey.asc "${tmp_folder}/${date}/passbolt-srvkeybkp_${date}.asc"
+docker cp passbolt-app-1:/etc/passbolt/gpg/serverkey.asc "${tmp_folder}/${date}/passbolt-srvkeybkp.asc" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup nginx.conf
-cp "/etc/nginx/conf.d/dev.komelt.passbolt.conf" "${tmp_folder}/${date}/nginx-confbkp_${date}.conf" 2>> "${tmp_folder}/${date}/log_${date}.log"
+cp "/etc/nginx/conf.d/dev.komelt.passbolt.conf" "${tmp_folder}/${date}/nginx-confbkp.conf" 2>> "${tmp_folder}/logs/${date}.log" 
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
 
 # backup serverkey.asc
-docker cp passbolt-app-1:/usr/share/php/passbolt/plugins/PassboltCe/WebInstaller/templates/Config/passbolt.php "${tmp_folder}/${date}/passbolt-confbkp_${date}.php"
+docker cp passbolt-app-1:/usr/share/php/passbolt/plugins/PassboltCe/WebInstaller/templates/Config/passbolt.php "${tmp_folder}/${date}/passbolt-confbkp.php" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
+
 # ------------------------------- BACKUP END
+
+
 
 # ------------------------------- SYNC FILES START
 
@@ -59,14 +65,12 @@ cd "${tmp_folder}"
 
 find "${tmp_folder}"/* -mtime +30 -maxdepth 0 -exec basename {} \; | xargs rm -r {}
 
-# Backblaze b2 authorize
-b2 authorize-account $BACKB_KEY_ID $BACKB_APP_KEY 2>> "${tmp_folder}/${date}/log_${date}.log"
+ssh -i "${HOME}/.ssh/id_rsa" root@192.168.1.18 mkdir -p "/data/${name}/config/" 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
 
-# Sync local cirectory with b2 bucket
-b2 sync --delete --compareVersions none "${tmp_folder}" "b2://zeus-docker-backup/${name}/" 2>> "${tmp_folder}/${date}/log_${date}.log"
+rsync -au "/appdata/tmp/${name}/" -e "ssh -i ${HOME}/.ssh/id_rsa" root@192.168.1.18:"/data/${name}/config/" --delete 2>> "${tmp_folder}/logs/${date}.log"
 if [[ $? -ne 0 ]]; then
     err=true
 fi
@@ -74,8 +78,9 @@ fi
 # ------------------------------- SYNC FILES END
 
 
-echo "" >> "${tmp_folder}/${date}/log_${date}.log"
-echo "" >> "${tmp_folder}/${date}/log_${date}.log"
+
+echo "" >> "${tmp_folder}/logs/${date}.log"
+echo "" >> "${tmp_folder}/logs/${date}.log"
 
 if [[ $err == "true" ]]; then
     exit 1
